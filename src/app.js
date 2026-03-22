480363bc1c33ed7659ce72afc955e9a076dff89e
rahwan10 <donggeon10@naver.com>
2026-03-22 20:18:50 +0900

    fix:안쓰는파일삭제

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import Restart from './controllers/Restart.js'
-import play from './controllers/Play.js'
-
-// const ComNum = pickRandomThreeNum();
-document.getElementById("submit").onclick = play;
-document.getElementById("game-restart-button").onclick=Restart
