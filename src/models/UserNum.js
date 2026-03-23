06a40ca87818f278979f45779926315009d7b52d
rahwan10 <donggeon10@naver.com>
2026-03-23 14:52:00 +0900

    fix:여러가지 입력값에 대한 경고 추가

--- src/models/UserNum.js
+++ src/models/UserNum.js
@@ -2,15 +2,23 @@ export default getUserNum
 
 function getUserNum() {
     let UserNum = Array.from(document.getElementById("user-input").value);
-    if (UserNum.length == 0) {
+    if (UserNum.length === 0) {
         alert("숫자를 적어주세요");
         return null;
-    } else if (UserNum.length < 3 || UserNum.length > 3) {
+    } else if (UserNum.length !== 3 ) {
         alert("숫자 3개를 붙여 적어주세요!");
         return null;
     }
     if (isDup(UserNum)) {
         alert("중복되지않는 숫자를 입력해주세요!");
+        return null;
+    }
+    if(isZero(UserNum)){
+        alert("1~9까지의 숫자를 입력해주세요!");
+        return null;
+    }
+    if(isNotNum(UserNum)){
+        alert("숫자를 입력해주세요!");
         return null;
     }
 
@@ -32,4 +40,22 @@ function isSame(UserNum, i, j, Isdup) {
     } else {
         return Isdup
     }
+}
+function isNotNum(UserNum){
+    let isNotNum=false
+    for(let i=0;i<3;i++){
+        if(isNaN(UserNum[i])){
+            isNotNum=true;
+        }
+    }
+    return isNotNum;
+}
+function isZero(UserNum){
+    let isZero=false;
+    for(let i=0;i<3;i++){
+        if(UserNum[i]==0){
+            isZero=true;
+        }
+    }
+    return isZero;
 }
